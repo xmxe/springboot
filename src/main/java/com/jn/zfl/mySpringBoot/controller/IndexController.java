564bcc9520a692d6fa33eeb56bd57748ee92e930
xmxe <464817304@qq.com>
2018-03-30 11:45:43 +0800

    基于bootstrap的ace后台管理模板

--- src/main/java/com/jn/zfl/mySpringBoot/controller/IndexController.java
+++ src/main/java/com/jn/zfl/mySpringBoot/controller/IndexController.java
@@ -3,8 +3,6 @@ package com.jn.zfl.mySpringBoot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.jn.zfl.mySpringBoot.config.quarts.QuartzManager;
 
 @Controller
