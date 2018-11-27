d3396def915f63ace07b4aa1fffab2ead2baeb04
xmxe <464817304@qq.com>
2018-11-27 14:35:03 +0800

    redis config

--- src/main/java/com/mySpringBoot/config/redis/RedisConfig.java
+++ src/main/java/com/mySpringBoot/config/redis/RedisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,7 +26,8 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
-@EnableCaching // 启用缓存，这个注解很重要；
+@EnableCaching // 启用缓存，这个注解很重要
+@PropertySource("classpath:redis.properties") 
 public class RedisConfig extends CachingConfigurerSupport {
 
      /**
