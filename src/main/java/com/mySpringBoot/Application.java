9e285d2eea1113e9039819aec6a022c1dbafcdc6
xmxe <464817304@qq.com>
2019-05-23 12:10:00 +0800

    mybatis配置

--- src/main/java/com/mySpringBoot/Application.java
+++ src/main/java/com/mySpringBoot/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan//springboot启动类扫描servlet组件(过滤器filter)
+//@MapperScan("com.mySpringBoot.dao")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
