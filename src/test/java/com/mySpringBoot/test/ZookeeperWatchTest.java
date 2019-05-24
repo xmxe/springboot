5e9287605e973e1ba5331497ded50573b450ee5d
xmxe <464817304@qq.com>
2019-05-24 16:18:51 +0800

    zk lambda

--- src/test/java/com/mySpringBoot/test/ZookeeperWatchTest.java
+++ src/test/java/com/mySpringBoot/test/ZookeeperWatchTest.java
@@ -24,14 +24,14 @@ class Wa implements Runnable {
 	public void run() {
 		// 连接启动k
 		try {
-			ZooKeeper zk = new ZooKeeper("192.168.236.129:2181", 60000, new Watcher() {
+			ZooKeeper zk = new ZooKeeper("192.168.236.128:2181", 60000, new Watcher() {
 				// 监控所有被触发的事件
 				public void process(WatchedEvent event) {
 					System.out.println("changing...");
 				}
 			});
 			// 设置监听器
-			Watcher wc = new Watcher() {
+		/*	Watcher wc = new Watcher() {
 				public void process(WatchedEvent event) {
 					// TODO Auto-generated method stub
 					if (event.getType() == EventType.NodeDataChanged) {
@@ -45,6 +45,19 @@ class Wa implements Runnable {
 					}
 				}
 
+			};*/
+			
+			Watcher wc = (event) -> {
+				if (event.getType() == EventType.NodeDataChanged) {
+					System.out.println("change");
+				}
+				if (event.getType() == EventType.NodeDeleted) {
+					System.out.println("dele");
+				}
+				if (event.getType() == EventType.NodeCreated) {
+					System.out.println("create");
+				}
+				
 			};
 			// 进行轮询，其中exists方法用来询问状态，并且设置了监听器，如果发生变化，则会回调监听器里的方法。
 			while (true) {
