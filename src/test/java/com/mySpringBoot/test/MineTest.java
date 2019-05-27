57a58c8458ed51711fea73599ca3d48df53b4dff
xmxe <464817304@qq.com>
2019-05-27 11:53:58 +0800

    二分查找

--- src/test/java/com/mySpringBoot/test/MineTest.java
+++ src/test/java/com/mySpringBoot/test/MineTest.java
@@ -58,7 +58,7 @@ public class MineTest {
 		logger.info("记录手动将信息输出到文件");
 	}
 	@Test
-    public void testSend1(){
+    public void testSend(){
 
         // 创建动态客户端
         JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
@@ -92,4 +92,38 @@ public class MineTest {
 		// 第二个参数内容：是在第多少时间之后开始执行定时任务内容，该参数不能小于0
 		// 第三个参数内容：是在两个任务之间的执行时间间隔，该参数不能小于等于0
 	}
+	
+	//https://mp.weixin.qq.com/s/HXF6rWJPeGyLLhiSa7zdog 定义一个整数数组，如何找出一个整数是否在这个数组里并获得整数的下标
+	@Test
+	public void 二分查找(){
+		int[] array = new int[1000];
+		for(int i = 0;i<1000;i++){
+			array[i]=i*14;
+		}
+		System.out.println(binarySearch(array,2422));
+	}
+	
+	
+	public int binarySearch(int[] array, int target) {
+		// 查找范围起点
+		int start = 0;
+		// 查找范围终点
+		int end = array.length - 1;
+		// 查找范围中位数
+		int mid;
+		// 迭代进行二分查找
+		while (start <= end) {
+			mid = (start + end) / 2;
+			if (array[mid] == target) {
+				return mid;
+			} else if (array[mid] < target) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	
 }
