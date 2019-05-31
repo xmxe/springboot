3904c82e0025c973454a06f3e34cbb4e66dd99a0
xmxe <464817304@qq.com>
2019-05-31 10:11:16 +0800

    增加RedisConfig 解决写入redis乱码的问题

---
+++ src/main/java/com/mySpringBoot/config/redis/RedisConfig.java
@@ -0,0 +1,62 @@
+package com.mySpringBoot.config.redis;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+@EnableCaching//开启缓存
+public class RedisConfig extends CachingConfigurerSupport{
+    
+    @Bean
+    public KeyGenerator keyGenerator() {
+     /*   return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };*/
+    	return (target,method,params)->{
+    		StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+    	};
+    }
+  
+    //通过自定义序列化，解决写入redis后乱码的问题
+    @Bean(name="redisTemplate")
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        template.setConnectionFactory(factory);
+        //key序列化方式
+        template.setKeySerializer(redisSerializer);
+        //value序列化
+        template.setValueSerializer(redisSerializer);
+        //value hashmap序列化
+        template.setHashValueSerializer(redisSerializer);
+        //key haspmap序列化
+        template.setHashKeySerializer(redisSerializer);
+        //
+        return template;
+    }
+
+
+}
